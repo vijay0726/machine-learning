801b6c860cb3f2a908618c3f686aea16a5c89d31
王杰 <2623312985@qq.com>
2021-11-11 22:34:33 +0800

    feat: 🎸 neural-networks

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+// const path = require('path')
+
+// module.exports = {
+//   entry: './script.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'bundle.js',
+//   },
+//   mode: 'development',
+// };
+
+const path = require('path');
+
+module.exports = {
+  entry: './script.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+//   mode: 'development',
+};
